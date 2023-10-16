9244ebfe97711b4ed9c9c50cafbbced388f1385b
Magdams <Magdams@MSI>
2023-10-16 09:19:28 +0200

    TDD Paso 1.2 -> Pruebas que fallan en Cuenta

--- src/pkg/Cuenta.java
+++ src/pkg/Cuenta.java
@@ -2,4 +2,18 @@ package pkg;
 
 public class Cuenta {
 
+	public void Ingresar(int i) {
+		
+	}
+
+	public Integer getSaldo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void Retirar(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
