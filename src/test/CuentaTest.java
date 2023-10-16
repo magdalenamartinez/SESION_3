a3c4755e07874ecb5ee6b1f0315a9d69f68aa22a
Magdams <Magdams@MSI>
2023-10-16 09:16:37 +0200

    TDD Paso 1 -> Crear Prueba Cuenta

---
+++ src/test/CuentaTest.java
@@ -0,0 +1,43 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CuentaTest {
+
+	static Cuenta aux;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testIngresar() {
+		aux.Ingresar(1000);
+		assertEquals(1000, aux.getSaldo());
+	}
+	
+	@Test
+	void testRetirar() {
+		aux.Retirar(100);
+		assertEquals(0, aux.getSaldo());
+	}
+
+}
