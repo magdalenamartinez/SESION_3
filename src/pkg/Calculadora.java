a98c512a9105219af6eb294dcf288a8f4e2fa8d4
Magdams <Magdams@MSI>
2023-10-16 09:02:36 +0200

    TDD Paso 1.2 -> Crea metodos auto Calculadora

--- src/pkg/Calculadora.java
+++ src/pkg/Calculadora.java
@@ -2,4 +2,24 @@ package pkg;
 
 public class Calculadora {
 
+	public static Integer suma(int i, int j) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static Integer resta(int i, int j) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static Integer multiplica(int i, int j) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static Integer divide(int i, int j) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
