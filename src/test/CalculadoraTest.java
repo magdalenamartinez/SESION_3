d5e5a0e00e906b28650f12a30b7196929f4e5ec7
Magdams <Magdams@MSI>
2023-10-16 09:01:20 +0200

    TDD Paso 1 -> Crear prueba Calculadora

---
+++ src/test/CalculadoraTest.java
@@ -0,0 +1,51 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import pkg.Calculadora;
+
+class CalculadoraTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testSuma() {
+		assertEquals(5, Calculadora.suma(3,2));
+	}
+	
+	@Test
+	void testResta() {
+		assertEquals(-1, Calculadora.resta(3,2));
+	}
+	
+	@Test
+	void testMultiplica() {
+		assertEquals(6, Calculadora.multiplica(3,2));
+	}
+	
+	@Test
+	void testDivide() {
+		assertEquals(1, Calculadora.divide(3,2));
+	}
+
+}
